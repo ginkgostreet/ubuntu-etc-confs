d39ffb97812ecce03080c5ae6bef0265ef574e0d
Dan O'Brien <dan@ginkgostreet.com>
2018-03-29 12:22:28 -0400

    Configure drush system-wide to use PHP 5.6

---
+++ profile.d/drush.sh
@@ -0,0 +1,4 @@
+# Set the default PHP version under which drush runs. Before you change this
+# globally, consider whether setting an environment variable for yourself is
+# a more appropriate solution.
+export DRUSH_PHP='/usr/bin/php5.6'
